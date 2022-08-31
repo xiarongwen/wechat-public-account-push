f1370cd7518a660c8a9ec82f1f74e2c95b83add0
夏荣文 <45868382+xiarongwen@users.noreply.github.com>
2022-08-31 17:38:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     APP_SECRET: "8c2bc36ef89891492c6d93c73d763bd1",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "v1dmF8yJriyZ9udsELgTh1jOyhVv7JOWs1Hrla9HYaE",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
@@ -25,14 +25,11 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+      {name: '鼻炎', id: "odISR6utpw6ubFPEKXnNevzGyyuc"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "odISR6utpw6ubFPEKXnNevzGyyuc"}, 
     ],
      
     /**
